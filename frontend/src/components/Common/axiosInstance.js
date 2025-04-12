9029f486ba322a9bdbc2ce499fb2195a918a3615
karAlpp <alpkover@gmail.com>
2025-04-12 15:04:02 +0300

    update: frontend axios baseURL and env configs

--- frontend/src/components/Common/axiosInstance.js
+++ frontend/src/components/Common/axiosInstance.js
@@ -1,8 +1,7 @@
-// src/utils/axiosInstance.js
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://acd-backend.onrender.com', // Render'dan gelen backend URL
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:9000/api',
   withCredentials: true,
 });
 
